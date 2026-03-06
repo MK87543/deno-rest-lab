6b21f1e831054ecbc0da6585e70b8a6dc965dbf8
Matteo Kleber <klema7b@gmail.com>
2026-03-06 15:53:36 +0100

    commit_01_test_upload

--- main.ts
+++ main.ts
@@ -6,6 +6,6 @@ const app = new Application();
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-console.log("Server running on http://localhost:8079/hello/matteo");
+console.log("Server runningv on http://localhost:8079/hello/matteo");
 
 await app.listen({ port: 8079 });
