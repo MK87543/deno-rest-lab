75868b582bec44ef6b23849d3293bdab6b7a1c52
Matteo Kleber <klema7b@gmail.com>
2026-03-06 15:46:03 +0100

    initial API

---
+++ main.ts
@@ -0,0 +1,11 @@
+import { Application } from "https://deno.land/x/oak/mod.ts";
+import router from "./routes.ts";
+
+const app = new Application();
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+console.log("Server running on http://localhost:8079/hello/matteo");
+
+await app.listen({ port: 8079 });
